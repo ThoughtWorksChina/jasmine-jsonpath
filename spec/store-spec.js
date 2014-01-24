d1ad8fd70a95f34155e983942d6ada627bdb3d68
Qiu Juntao <juntao.qiu@gmail.com>
2014-01-25 00:01:35 +1100

    add a more powerful matcher . Pair: Kangping

--- spec/store-spec.js
+++ spec/store-spec.js
@@ -15,7 +15,12 @@ describe("jasmine jsonpath", function() {
                     type: "Dynamic",
                     samples: ["JavaScript"]
                 }
-            ]
+            ],
+            tools: {
+                category: {
+                    name: "Common"
+                }
+            }
         };
     });
 
@@ -28,6 +33,32 @@ describe("jasmine jsonpath", function() {
     it("should be able to deteming a path is existing", function() {
         expect(obj).toHasJsonPath('$.languages[0].type');
         expect(obj).toHasJsonPath('$.languages[?(@.type=="Static")]');
+    });
+
+    it("should be able to tell an object has a special schema", function() {
+        var schema = {
+            languages: [
+                {
+                    type: "Static",
+                    samples: []
+                }
+            ]
+        };
+
+        expect(obj).toHasSchema(schema);
+    });
+
+    it("should be able to tell an object has a special schema, more complex", function() {
+        var schema = {
+            languages: [],
+            tools: {
+                category: {
+                    name: "Common"
+                }
+            }
+        };
+
+        expect(obj).toHasSchema(schema);
     });
 });
 
